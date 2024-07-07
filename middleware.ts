1df4a7fa65a5aa592f253eff0865db090aa0fbcf
tianbao <mtian999@163.com>
2024-07-07 11:52:08 +0800

    fix: 修改middleware 正則匹配

--- middleware.ts
+++ middleware.ts
@@ -16,5 +16,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next)(?!.*\\.(?:ico|png|svg|jpg|jpeg|xml|txt)$)(?!/api).*)"],
+  matcher: ["/((?!api|_next|.*\\..*).*)"],
 };
