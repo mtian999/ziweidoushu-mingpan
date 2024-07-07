30a2160f67a7b542acbd50819db835f03fc9b355
tianbao <mtian999@163.com>
2024-07-07 11:11:42 +0800

    feat: 滚动图标改成宫位十二地支

--- config/logos.ts
+++ config/logos.ts
@@ -1,35 +1,50 @@
-
 export const LOGOS = [
   {
-    name: "Next.js",
-    image: "/images/techStack/nextjs.svg",
+    name: "dizhi-0-yin",
+    image: "/images/techStack/dizhi-0-yin.svg",
   },
   {
-    name: "React",
-    image: "/images/techStack/react.svg",
+    name: "dizhi-1-mao",
+    image: "/images/techStack/dizhi-1-mao.svg",
   },
   {
-    name: "Tailwind",
-    image: "/images/techStack/tailwind.svg",
+    name: "dizhi-2-chen",
+    image: "/images/techStack/dizhi-2-chen.svg",
   },
   {
-    name: "Framer",
-    image: "/images/techStack/framer.svg",
+    name: "dizhi-3-si",
+    image: "/images/techStack/dizhi-3-si.svg",
   },
   {
-    name: "Shadcnui",
-    image: "/images/techStack/shadcnui.svg",
+    name: "dizhi-4-wu",
+    image: "/images/techStack/dizhi-4-wu.svg",
   },
   {
-    name: "Nextui",
-    image: "/images/techStack/nextui.svg",
+    name: "dizhi-5-wei",
+    image: "/images/techStack/dizhi-5-wei.svg",
   },
   {
-    name: "TS",
-    image: "/images/techStack/typescript.svg",
+    name: "dizhi-6-shen",
+    image: "/images/techStack/dizhi-6-shen.svg",
   },
   {
-    name: "Vercel",
-    image: "/images/techStack/vercel.svg",
+    name: "dizhi-7-you",
+    image: "/images/techStack/dizhi-7-you.svg",
+  },
+  {
+    name: "dizhi-8-xu",
+    image: "/images/techStack/dizhi-8-xu.svg",
+  },
+  {
+    name: "dizhi-9-hai",
+    image: "/images/techStack/dizhi-9-hai.svg",
+  },
+  {
+    name: "dizhi-10-zi",
+    image: "/images/techStack/dizhi-10-zi.svg",
+  },
+  {
+    name: "dizhi-11-chou",
+    image: "/images/techStack/dizhi-11-chou.svg",
   },
 ];
