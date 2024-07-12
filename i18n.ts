7a68b7eea97b6bd13dfc5ba0b38b9a984c1ed6cf
tianbao <mtian999@163.com>
2024-07-12 19:56:54 +0800

    feat:增加404回收页SEO

--- i18n.ts
+++ i18n.ts
@@ -8,5 +8,5 @@ export default getRequestConfig(async ({ locale }) => {
   // Validate that the incoming `locale` parameter is valid
   if (!locales.includes(locale as any)) notFound();
 
-  return {};
+  return { messages: (await import(`./locales/${locale}.json`)).default };
 });
